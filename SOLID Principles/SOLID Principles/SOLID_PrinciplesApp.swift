411798b842b3b925fcbfa5313482fc42a880f190
Jacob Davis <jacobdofficial@gmail.com>
2024-01-30 09:47:55 -0700

    Initial Setup

---
+++ SOLID Principles/SOLID Principles/SOLID_PrinciplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLID_PrinciplesApp.swift
+//  SOLID Principles
+//
+//  Created by Jacob Davis on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SOLID_PrinciplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
